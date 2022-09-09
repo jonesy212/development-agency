3423093c593eb9be40d1ece6e93b5134844697e1
Marcus Jones <indelibleink84@gmail.com>
2022-09-09 14:01:55 -0500

    Created theme-tab-javascript
    Added change in size to arrow when hovered
    Created Variables for theme:tab, dark, light, open,
    active, data-array, switchBtn
    Tied event listener for click events open and close.

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
+const theme = 'theme';
+const dataTheme = '[data-theme]';
+const themeTab = '.theme-tab';
+const switcherBtn = '.switcher-btn';
+const dark = 'dark';
+const light = 'light';
+const open = 'open';
+const active = 'active';
+
+
+//store selector
+const root = document.documentElement;
+
+// Theme
+const toggleTheme = document.querySelector(themeTab);
+const switcher = document.querySelectorAll(switcherBtn);
+const currentTheme = localStorage.getItem(theme);
+
 //create more data attributes to select 
 const modalOpen = '[data-open]';
 const modalClose = '[data-close]';
 const isVisible = 'is-visible'
 
+//create open class
+//change the chevron direction based on open and close
+toggleTheme.addEventListener('click', function () {
+    const tab = this.parentElement.parentElement;
+    //check for class and then open
+    if ('click') {
+        console.log('clicked')
+    }
+    if (!tab.className.includes(open)) {
+        tab.classList.add(open)
+    } else {
+        tab.classList.remove(open)
+    }
+});
+
+
+// Modal
 //find every button to query everything
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
