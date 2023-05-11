150aed95e203c4782a423ff88d136bdee98abdc8
Marcus Jones <indelibleink84@gmail.com>
2023-05-11 03:43:32 -0500

    added email form; added images for contact and about section

--- js/form.js
+++ js/form.js
@@ -1,10 +1,12 @@
-const form = document.querySelector('.email-me form');
+const form = document.querySelector('#contact-form');
+const subject = form.querySelector('#subject').value;
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const name = form.querySelector('#name').value;
+  const email = form.querySelector('#email').value;
+  const subject = form.querySelector('#subject').value;
   const message = form.querySelector('#message').value;
-  const emailBody = `Name: ${name}%0D%0A%0D%0AMessage: ${message}`;
-  window.location.href = `mailto:youremail@yourdomain.com?subject=New%20Email%20From%20${name}&body=${emailBody}`;
+  const emailBody = `Subject: ${subject}%0D%0A%0D%0AName: ${name}%0D%0A%0D%0AEmail: ${email}%0D%0A%0D%0AMessage: ${message}`;
+  window.location.href = `mailto:energyiskey@protonmail.com?subject=New%20Email%20From%20${name}&body=${emailBody}`;
 });
-
-
